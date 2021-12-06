20b42e474ef638c0cce737a8cc449c8fa6e1d111
ennioitaliano <ennio.italiano@gmail.com>
2021-12-06 22:28:17 +0100

    Updated a little bit of code

--- app/src/main/java/com/eit/ipsc/Calcoli.kt
+++ app/src/main/java/com/eit/ipsc/Calcoli.kt
@@ -1,5 +1,7 @@
 package com.eit.ipsc
 
+import kotlin.math.pow
+
 class Calcoli(private val ip1Int: Int?, private val ip2Int: Int?, private val ip3Int: Int?, private val ip4Int: Int?, private val mascheraInt: Int?) : MainActivity_Calculate() {
 
     @Throws(NullPointerException::class)
@@ -10,55 +12,60 @@ class Calcoli(private val ip1Int: Int?, private val ip2Int: Int?, private val ip
 
         val arraybid = arrayOf(intArrayOf(128, 0), intArrayOf(64, 0), intArrayOf(32, 0), intArrayOf(16, 0), intArrayOf(8, 0), intArrayOf(4, 0), intArrayOf(2, 0), intArrayOf(1, 0))
 
-        if (mascheraInt!! >= 0 && mascheraInt <= 8) {
+        when {
+            mascheraInt!! in 0..8 -> {
 
-            for (i in 0..7)
-                arraybid[i][1] = Character.getNumericValue(ipBinarioArray()[0][i])
+                for (i in 0..7)
+                    arraybid[i][1] = Character.getNumericValue(ipBinarioArray()[0][i])
 
-            for (y in 0 until mascheraInt)
-                if (arraybid[y][1] == 1)
-                    somma += arraybid[y][0]
+                for (y in 0 until mascheraInt)
+                    if (arraybid[y][1] == 1)
+                        somma += arraybid[y][0]
 
-            indirizzoRete[0] = somma
+                indirizzoRete[0] = somma
 
-        } else if (mascheraInt in 9..16) {
+            }
+            mascheraInt in 9..16 -> {
 
-            for (i in 0..7)
-                arraybid[i][1] = Character.getNumericValue(ipBinarioArray()[1][i])
+                for (i in 0..7)
+                    arraybid[i][1] = Character.getNumericValue(ipBinarioArray()[1][i])
 
-            for (y in 0 until mascheraInt - 8)
-                if (arraybid[y][1] == 1)
-                    somma += arraybid[y][0]
+                for (y in 0 until mascheraInt - 8)
+                    if (arraybid[y][1] == 1)
+                        somma += arraybid[y][0]
 
-            indirizzoRete[0] = ip1Int!!
-            indirizzoRete[1] = somma
+                indirizzoRete[0] = ip1Int!!
+                indirizzoRete[1] = somma
 
-        } else if (mascheraInt in 17..24) {
+            }
+            mascheraInt in 17..24 -> {
 
-            for (i in 0..7)
-                arraybid[i][1] = Character.getNumericValue(ipBinarioArray()[2][i])
+                for (i in 0..7)
+                    arraybid[i][1] = Character.getNumericValue(ipBinarioArray()[2][i])
 
-            for (y in 0 until mascheraInt - 16)
-                if (arraybid[y][1] == 1)
-                    somma += arraybid[y][0]
+                for (y in 0 until mascheraInt - 16)
+                    if (arraybid[y][1] == 1)
+                        somma += arraybid[y][0]
 
-            indirizzoRete[0] = ip1Int!!
-            indirizzoRete[1] = ip2Int!!
-            indirizzoRete[2] = somma
+                indirizzoRete[0] = ip1Int!!
+                indirizzoRete[1] = ip2Int!!
+                indirizzoRete[2] = somma
 
-        } else if (mascheraInt in 25..32) {
+            }
+            mascheraInt in 25..32 -> {
 
-            for (i in 0..7)
-                arraybid[i][1] = Character.getNumericValue(ipBinarioArray()[3][i])
+                for (i in 0..7)
+                    arraybid[i][1] = Character.getNumericValue(ipBinarioArray()[3][i])
 
-            for (y in 0 until mascheraInt - 24)
-                if (arraybid[y][1] == 1)
-                    somma += arraybid[y][0]
+                for (y in 0 until mascheraInt - 24)
+                    if (arraybid[y][1] == 1)
+                        somma += arraybid[y][0]
 
-            indirizzoRete[0] = ip1Int!!
-            indirizzoRete[1] = ip2Int!!
-            indirizzoRete[2] = ip3Int!!
-            indirizzoRete[3] = somma
+                indirizzoRete[0] = ip1Int!!
+                indirizzoRete[1] = ip2Int!!
+                indirizzoRete[2] = ip3Int!!
+                indirizzoRete[3] = somma
+            }
         }
 
         return indirizzoRete
@@ -69,7 +76,7 @@ class Calcoli(private val ip1Int: Int?, private val ip2Int: Int?, private val ip
         return indirizzoReteArray()[0].toString() + "." + indirizzoReteArray()[1] + "." + indirizzoReteArray()[2] + "." + indirizzoReteArray()[3] + " /" + mascheraInt
     }
 
-    fun ipBinarioArray(): Array<String> {
+    private fun ipBinarioArray(): Array<String> {
 
         val ipBinFinal = arrayOf("", "", "", "")
 
@@ -85,7 +92,7 @@ class Calcoli(private val ip1Int: Int?, private val ip2Int: Int?, private val ip
         return ipBinarioArray()[0] + "." + ipBinarioArray()[1] + "." + ipBinarioArray()[2] + "." + ipBinarioArray()[3]
     }
 
-    fun mascheraArray(): IntArray {
+    private fun mascheraArray(): IntArray {
         val risultato: Int
         var mascConvertita = 0
         val mascheraFinal = intArrayOf(0, 0, 0, 0)
@@ -94,7 +101,7 @@ class Calcoli(private val ip1Int: Int?, private val ip2Int: Int?, private val ip
         if (mascheraInt!! >= 0 && mascheraInt <= 8) {
 
             tmp = 8 - mascheraInt
-            risultato = Math.pow(2.0, tmp.toDouble()).toInt()
+            risultato = 2.0.pow(tmp.toDouble()).toInt()
 
             mascConvertita = 255 - risultato + 1
 
@@ -103,17 +110,17 @@ class Calcoli(private val ip1Int: Int?, private val ip2Int: Int?, private val ip
         } else if (mascheraInt in 9..16) {
 
             tmp = 16 - mascheraInt
-            risultato = Math.pow(2.0, tmp.toDouble()).toInt()
+            risultato = 2.0.pow(tmp.toDouble()).toInt()
 
             mascConvertita = 255 - risultato + 1
 
             mascheraFinal[0] = 255
             mascheraFinal[1] = mascConvertita
 
-        } else if (mascheraInt >= 17 && mascheraInt <= 24) {
+        } else if (mascheraInt in 17..24) {
 
             tmp = 24 - mascheraInt
-            risultato = Math.pow(2.0, tmp.toDouble()).toInt()
+            risultato = 2.0.pow(tmp.toDouble()).toInt()
 
             mascConvertita = 255 - risultato + 1
 
@@ -121,10 +128,10 @@ class Calcoli(private val ip1Int: Int?, private val ip2Int: Int?, private val ip
             mascheraFinal[1] = 255
             mascheraFinal[2] = mascConvertita
 
-        } else if (mascheraInt >= 25 && mascheraInt <= 32) {
+        } else if (mascheraInt in 25..32) {
 
             tmp = 32 - mascheraInt
-            risultato = Math.pow(2.0, tmp.toDouble()).toInt()
+            risultato = 2.0.pow(tmp.toDouble()).toInt()
 
             mascConvertita = 255 - risultato + 1
 
@@ -159,11 +166,11 @@ class Calcoli(private val ip1Int: Int?, private val ip2Int: Int?, private val ip
 
     fun numIndDisp(): String {
 
-        var num: Long
-        val potenza3 = Math.pow(256.0, 3.0).toLong()
-        val potenza2 = Math.pow(256.0, 2.0).toLong()
-        val potenza1 = Math.pow(256.0, 1.0).toLong()
-        val potenza0 = Math.pow(256.0, 0.0).toLong()
+        val num: Long
+        val potenza3 = 256.0.pow(3.0).toLong()
+        val potenza2 = 256.0.pow(2.0).toLong()
+        val potenza1 = 256.0.pow(1.0).toLong()
+        val potenza0 = 256.0.pow(0.0).toLong()
 
         val broadcastArray0 = broadcastArray()[0]
         val broadcastArray1 = broadcastArray()[1]
@@ -178,15 +185,15 @@ class Calcoli(private val ip1Int: Int?, private val ip2Int: Int?, private val ip
         val calcolo1 = broadcastArray0 * potenza3 + broadcastArray1 * potenza2 + broadcastArray2 * potenza1 + (broadcastArray3 - 1) * potenza0
         val calcolo2 = indReteArray0 * potenza3 + indReteArray1 * potenza2 + indReteArray2 * potenza1 + (indReteArray3 + 1) * potenza0
 
-        if (mascheraInt == 32)
-            num = 1
+        num = if (mascheraInt == 32)
+            1
         else
-            num = calcolo1 - calcolo2 + 1
+            calcolo1 - calcolo2 + 1
 
         return num.toString()
     }
 
-    fun broadcastArray(): IntArray {
+    private fun broadcastArray(): IntArray {
 
         val broadcastFinal = intArrayOf(255, 255, 255, 255)
 
@@ -194,61 +201,67 @@ class Calcoli(private val ip1Int: Int?, private val ip2Int: Int?, private val ip
 
         val arraybid = arrayOf(intArrayOf(128, 0), intArrayOf(64, 0), intArrayOf(32, 0), intArrayOf(16, 0), intArrayOf(8, 0), intArrayOf(4, 0), intArrayOf(2, 0), intArrayOf(1, 0))
 
-        if (mascheraInt!! in 0..8) {
+        when {
+            mascheraInt!! in 0..8 -> {
 
-            for (i in 0..7)
-                arraybid[i][1] = Character.getNumericValue(ipBinarioArray()[0][i])
+                for (i in 0..7)
+                    arraybid[i][1] = Character.getNumericValue(ipBinarioArray()[0][i])
 
-            for (y in 0 until mascheraInt)
-                if (arraybid[y][1] == 0)
-                    somma += arraybid[y][0]
+                for (y in 0 until mascheraInt)
+                    if (arraybid[y][1] == 0)
+                        somma += arraybid[y][0]
 
-            broadcastFinal[0] = 255 - somma
+                broadcastFinal[0] = 255 - somma
 
-        } else if (mascheraInt in 9..16) {
+            }
+            mascheraInt in 9..16 -> {
 
-            for (i in 0..7)
-                arraybid[i][1] = Character.getNumericValue(ipBinarioArray()[1][i])
+                for (i in 0..7)
+                    arraybid[i][1] = Character.getNumericValue(ipBinarioArray()[1][i])
 
-            for (y in 0 until mascheraInt - 8)
-                if (arraybid[y][1] == 0)
-                    somma += arraybid[y][0]
+                for (y in 0 until mascheraInt - 8)
+                    if (arraybid[y][1] == 0)
+                        somma += arraybid[y][0]
 
-            broadcastFinal[0] = ip1Int!!
-            broadcastFinal[1] = 255 - somma
+                broadcastFinal[0] = ip1Int!!
+                broadcastFinal[1] = 255 - somma
 
-        } else if (mascheraInt in 17..24) {
+            }
+            mascheraInt in 17..24 -> {
 
-            for (i in 0..7)
-                arraybid[i][1] = Character.getNumericValue(ipBinarioArray()[2][i])
+                for (i in 0..7)
+                    arraybid[i][1] = Character.getNumericValue(ipBinarioArray()[2][i])
 
-            for (y in 0 until mascheraInt - 16)
-                if (arraybid[y][1] == 0)
-                    somma += arraybid[y][0]
+                for (y in 0 until mascheraInt - 16)
+                    if (arraybid[y][1] == 0)
+                        somma += arraybid[y][0]
 
-            broadcastFinal[0] = ip1Int!!
-            broadcastFinal[1] = ip2Int!!
-            broadcastFinal[2] = 255 - somma
+                broadcastFinal[0] = ip1Int!!
+                broadcastFinal[1] = ip2Int!!
+                broadcastFinal[2] = 255 - somma
 
-        } else if (mascheraInt in 25..31) {
+            }
+            mascheraInt in 25..31 -> {
 
-            for (i in 0..7)
-                arraybid[i][1] = Character.getNumericValue(ipBinarioArray()[3][i])
+                for (i in 0..7)
+                    arraybid[i][1] = Character.getNumericValue(ipBinarioArray()[3][i])
 
-            for (y in 0 until mascheraInt - 24)
-                if (arraybid[y][1] == 0)
-                    somma += arraybid[y][0]
+                for (y in 0 until mascheraInt - 24)
+                    if (arraybid[y][1] == 0)
+                        somma += arraybid[y][0]
 
-            broadcastFinal[0] = ip1Int!!
-            broadcastFinal[1] = ip2Int!!
-            broadcastFinal[2] = ip3Int!!
-            broadcastFinal[3] = 255 - somma
+                broadcastFinal[0] = ip1Int!!
+                broadcastFinal[1] = ip2Int!!
+                broadcastFinal[2] = ip3Int!!
+                broadcastFinal[3] = 255 - somma
 
-        } else if (mascheraInt == 32) {
-            broadcastFinal[0] = ip1Int!!
-            broadcastFinal[1] = ip2Int!!
-            broadcastFinal[2] = ip3Int!!
-            broadcastFinal[3] = ip4Int!!
+            }
+            mascheraInt == 32 -> {
+                broadcastFinal[0] = ip1Int!!
+                broadcastFinal[1] = ip2Int!!
+                broadcastFinal[2] = ip3Int!!
+                broadcastFinal[3] = ip4Int!!
+            }
         }
 
         return broadcastFinal
